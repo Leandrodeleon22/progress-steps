af22636a71ad3e055611e7ca9fde3f458bb70922
Leandrodeleon22 <leandrodeleon22@gmail.com>
2022-05-02 16:48:08 +0800

    button active

---
+++ script.js
@@ -0,0 +1,47 @@
+const progress = document.getElementById("progress");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", function () {
+  currentActive++;
+  if (currentActive >= circles.length) {
+    currentActive = circles.length;
+  }
+
+  if (currentActive > 1 || circles.length - 1) {
+    prev.classList.remove("btn-disable");
+  }
+  if (currentActive === circles.length) {
+    next.classList.add("btn-disable");
+  }
+  update();
+  console.log(currentActive);
+});
+
+prev.addEventListener("click", function () {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  if (currentActive > 1 || circles.length - 1) {
+    prev.classList.remove("btn-disable");
+    next.classList.remove("btn-disable");
+  }
+  if (currentActive === 1) {
+    next.classList.remove("btn-disable");
+    prev.classList.add("btn-disable");
+  }
+  update();
+  console.log(currentActive);
+});
+
+const update = function () {
+  circles.forEach(function (circle, index) {
+    circle.classList.remove("circle-active");
+  });
+  circles[currentActive - 1].classList.add("circle-active");
+};
